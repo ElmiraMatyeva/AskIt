0083f21d45b1eebfb1ebc88d5cd637427bf82806
Elmira Matyeva <elmira.matyeva@gmail.com>
2022-03-25 22:32:09 +0600

    Added session creation (User Log In)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
- resources :users, only: %i[new create]
- resources :questions do 
-   resources :answers, except: %i[new show]
+  resources :sessions, only: %i[new create destroy]
+  resources :users, only: %i[new create]
+  resources :questions do 
+  resources :answers, except: %i[new show]
  end
  root 'pages#index'
 end
