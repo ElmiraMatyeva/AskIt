85da72151568f25a523ac7955315401916ab7db4
Elmira Matyeva <elmira.matyeva@gmail.com>
2022-03-25 17:16:50 +0600

    Generated User model. Started User registration (without Devise)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
- root 'pages#index'
+ resources :users, only: %i[new create]
  resources :questions do 
    resources :answers, except: %i[new show]
  end
+ root 'pages#index'
 end
