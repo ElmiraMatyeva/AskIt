425247158d5c03585f5b346aa35bbc333ce5d85f
Elmira Matyeva <elmira.matyeva@gmail.com>
2022-03-26 02:07:15 +0600

    Made it possible for the user to enter the (correct) old password while updating the user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,46 @@
 class User < ApplicationRecord
-  has_secure_password
+  attr_accessor :old_password
+    # чтобы создать виртуальный атрибут для юзера (он не будет попадать в базу данных, 
+    # а просто на объекте User будет сущ-ть этот атрибут), с его помощью мы будем 
+    # отрисовывать поле для ввода старого пароля
+  
+    # отменяем валидации от гема bcrypt, чтобы прописать необходимые валидации самостоятельно  
+  has_secure_password validations: false
+  validate  :password_presence
+  validate  :password_complexity
+  validate  :correct_old_password, on: :update
+
+  validates :password, confirmation: true, allow_blank: true
+    # allow_blank: true означает, что при обновлении профиля юзер может не захотеть 
+    # вводить пароль снова (то есть, мы разрешаем оставить поле пароля пустым)
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  
+  private 
 
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+
+    errors.add :password, 'complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
+  def password_presence
+    # нужно добавить для пароля warning, что он пустой, но только не в том случае, 
+    # если password_digest уже был указан. То есть, password_digest.present? 
+    # означает, что Юзер уже указал пароль раньше. Значит, при обновлении профиля 
+    # он может указать пароль, а может и оставить поле пустым.
+    errors.add(:password, :blank) unless password_digest.present?
+  end
+
+  def correct_old_password
+    # password_digest_was спец.метод RoR, означает, что надо вытащить именно старый 
+    # пароль, который в бд хранится, а не новый, который хранится в памяти. 
+    # is_password?(old_password) значит мы сделаем digest на основе старого пароля
+    # и сравним с digest, который хранится в бд
+    return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
+
+    errors.add :old_password, 'is incorrect'
+  end
 end
